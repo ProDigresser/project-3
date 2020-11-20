813902cc10ca8aabe9803841052f14d914edbc14
Laurence Walshe <laurence_walshe@hotmail.com>
2020-11-20 15:52:34 +0000

    deployment 6

--- backend/seed.js
+++ backend/seed.js
@@ -5,6 +5,7 @@ const Message = require('./models/message')
 const Social = require('./models/social')
 const { dbURI } = require('./config/environment')
 
+
 mongoose.connect(
   dbURI,
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
